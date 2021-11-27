55ce009faa8c12a08a918c24956b0f1aed09db27
ItShop <ItShop@DESKTOP-CTGQ2OD>
2021-11-27 20:18:22 +0200

    Add project files.

---
+++ 0remove0/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0remove0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> MyList = new LinkedList<string>();
+            
+            MyList.AddFirst("OK");
+            MyList.AddLast("K.O");
+            LinkedListNode<String> MyNode = new LinkedListNode<string>("Done");
+            MyList.AddFirst(MyNode);
+            MyList.AddBefore(MyNode, "Tool");
+            MyList.AddAfter(MyNode, "Cool");
+
+            Console.WriteLine("The LinkedList Before Deleton: ");
+            
+            foreach(var item in MyList)
+            {
+                Console.WriteLine(item);
+            }
+
+            MyList.RemoveFirst();
+            MyList.Remove("OK");
+            MyList.RemoveLast();
+
+            Console.WriteLine("The LinkedList After Deletion: ");
+
+            foreach (var item in MyList)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
